c2b21c49c28d0cabd862b16f367edcadc24451fd
marcelodalves <43051943+marcelodalves@users.noreply.github.com>
2019-03-28 20:03:15 -0300

    Create ERRADO.c

---
+++ ERRADO.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int calcsoma(int a, int b){
+int i, soma;
+for (i=a+1; i<b; i++) { 
+i +=soma;
+}
+}
+int main(){
+	int i,n1, n2, soma;
+	printf("\nEntre com o primeiro numero:\n");
+	scanf("%d",&n1);
+	
+	printf("\nEntre com o segundo numero:\n");
+	scanf("%d",&n2);
+	
+	printf("Soma = %d",soma);
+		
+	return 0;
+}
